2d5839757d711830c1573c275490f9ecf59d53d8
Sultan Mustafin <sultan.fb29@gmail.com>
2022-11-16 16:06:56 +0300

    feat: create ideas seeder

--- database/seeds/methods/seed-database.ts
+++ database/seeds/methods/seed-database.ts
@@ -1,3 +1,4 @@
+import { seedIdeas } from '../seeders/ideas.seeder';
 import { seedUsers } from '../seeders/users.seeder';
 import { getAndInitializeAppDataSource } from './get-and-initialize-app-data-source';
 
@@ -5,6 +6,7 @@ export const seedDatabase = async () => {
   const AppDataSource = await getAndInitializeAppDataSource();
 
   await seedUsers(AppDataSource);
+  await seedIdeas(AppDataSource);
   console.log('\nDatabase seeded');
   process.exit(0);
 };
