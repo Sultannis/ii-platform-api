d24789f67f9ac2287befbed433a8894073f6d303
Sultan Mustafin <sultan.fb29@gmail.com>
2022-12-28 18:27:13 +0300

    feat: install nest js swagger package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +9,14 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('ii platform API')
+    .setDescription('RESTful API for ii-platform project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
