a97312c812b2e863e8a597b62bd855bffbd02823
Sultan Mustafin <sultan.fb29@gmail.com>
2023-01-01 23:52:49 +0300

    feat: create user saved idea dao

---
+++ src/common/dao/user-saved-idea.dao.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user_saved_ideas')
+export class UserSavedIdeaDao {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'bigint' })
+  id: number;
+
+  @Column({ name: 'user_id', type: 'bigint' })
+  userId: number;
+
+  @Column({ name: 'idea_id', type: 'bigint' })
+  ideaId: number;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamptz', nullable: true })
+  deletedAt?: string;
+}
