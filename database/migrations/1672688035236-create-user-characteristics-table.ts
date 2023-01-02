c1356e78c265ab601fadeb316c99e3f6efbd3ba2
Sultan Mustafin <sultan.fb29@gmail.com>
2023-01-02 22:39:58 +0300

    feat: create user characteristics table

---
+++ database/migrations/1672688035236-create-user-characteristics-table.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUserCharacteristicsTable1672688035236
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_characteristics',
+        columns: [
+          {
+            name: 'id',
+            isPrimary: true,
+            isGenerated: true,
+            type: 'bigint',
+          },
+          {
+            name: 'user_id',
+            type: 'bigint',
+          },
+          {
+            name: 'characteristic_id',
+            type: 'bigint',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['user_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+          },
+          {
+            columnNames: ['characteristic_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'characteristics',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_characteristics');
+  }
+}
