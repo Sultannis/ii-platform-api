2aee556325c1341decdc8a0bcb21d0a98721d17f
Sultan Mustafin <sultan.fb29@gmail.com>
2023-01-11 23:51:59 +0300

    fix: include work companies repository into work companies module

--- src/modules/work-companies/work-companies.module.ts
+++ src/modules/work-companies/work-companies.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WorkCompaniesService } from './work-companies.service';
 import { WorkCompaniesController } from './accessor/work-companies.controller';
+import { WorkCompaniesRepository } from './work-companies.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkCompanyDao } from 'src/common/dao/work-company.dao';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WorkCompanyDao])],
   controllers: [WorkCompaniesController],
-  providers: [WorkCompaniesService]
+  providers: [WorkCompaniesService, WorkCompaniesRepository],
 })
 export class WorkCompaniesModule {}
