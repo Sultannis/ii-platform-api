0ad2cf40887d73857c06a99897f486e32d647a64
Sultan Mustafin <sultan.fb29@gmail.com>
2023-01-20 19:50:53 +0300

    feat: return user's avatar_url parameter

--- src/modules/users/accessor/resources/user.resource.ts
+++ src/modules/users/accessor/resources/user.resource.ts
@@ -22,6 +22,7 @@ export class UserResource {
       residence_city: user.residenceCity,
       occupation: user.occupation,
       bio: user.bio,
+      avatar_url: user.avatarUrl,
       role: +user.role,
       created_at: user.createdAt,
       updated_at: user.updatedAt,
