fd13eb167474031b8bcab7eb9b6058ed04b1c0d1
Sultan Mustafin <sultan.fb29@gmail.com>
2023-01-25 01:11:22 +0300

    feat: create user contact list at users seeder

--- database/seeds/seeders/users.seeder.ts
+++ database/seeds/seeders/users.seeder.ts
@@ -2,11 +2,14 @@ import { DataSource } from 'typeorm';
 import { generateUser } from '../generators/user.generator';
 
 export const seedUsers = async (dataSource: DataSource) => {
-  const repository = dataSource.getRepository('UserDao');
+  const usersRepository = dataSource.getRepository('UserDao');
+  const contactListsRepository = dataSource.getRepository('ContactListDao');
 
   try {
     for (let i = 0; i < 40; i++) {
-      await repository.insert(generateUser());
+      const user = usersRepository.create(generateUser());
+      await usersRepository.save(user);
+      await contactListsRepository.insert({ userId: user.id });
     }
   } catch (error: unknown) {
     console.log(`Error: Failed to seed Users`);
